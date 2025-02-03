b7a8839dfb6c529eaa469ec602906228db166fad
ChihiroSuda <c.suda.24.4.1380@4nonome.com>
2025-02-03 20:30:26 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,20 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
 
 # TODO
+n = int(input)
+
+if n < 2:
+    print(f"{n}は素数ではありません。")
+
+else:
+    prime = True
+    
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i ==0:
+            prime = False
+            break
+
+if prime:
+    print(f"{n}は素数です。")
+
+else:
+    print(f"{n}は素数ではありません。")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,21 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+PI = 3.141592653589793
+def sin_integral(a, b, N):
+    h = (b - a) / N
+    integral = 0
+
+    for k in range (1, N + 1):
+        x_k_1 = a + (k - 1) * h
+        x_k = a + k * h
+        integral += sin(x_k_1) + sin(x_k)
+    integral *= h / 2
+    return integral
+a = 0
+b = PI / 2
+N = 100
+
+result = sin_integral(a, b, N)
+
+print(f"台数積分法による近似積分値：{result}")
