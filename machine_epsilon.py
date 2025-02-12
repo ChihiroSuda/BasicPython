7c962e990d815af67a2b19b5693db7bb742116de
ChihiroSuda <chihiro.s6329@gmail.com>
2025-02-12 11:20:56 +0900

    Revert "制御構文の解答"

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,9 +1,1 @@
 # TODO
-epsilon = 1.0
-
-while 1.0 + epsilon > 1.0:
-    epsilon /= 2
-
-epsilon *= 2
-
-print(f"マシンイプシロン：{epsilon}")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,22 +1,5 @@
-from math import sin, pi
+from math import sin
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
-def sin_integral(a, b, N):
-    h = (b - a) / N
-    integral = 0
-
-    for k in range (1, N + 1):
-        x_k_1 = a + (k - 1) * h
-        x_k = a + k * h
-        integral += sin(x_k_1) + sin(x_k)
-    integral *= h / 2
-    return integral
-a = 0
-b = pi / 2
-N = 100
-
-result = sin_integral(a, b, N)
-
-print(f"台数積分法による近似積分値：{result}")
